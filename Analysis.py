b01ec11c9f24b83eaab0a9d84c37d075c7a58ffa
Manikanta <badugumanikanta1603@gmail.com>
2022-06-07 12:32:09 +0200

    updating

--- Analysis.py
+++ Analysis.py
@@ -10,5 +10,5 @@ all_months_data = pd.DataFrame()
 for file in files:
     df = pd.read_csv(directory_csv_files.format(file))
     all_months_data = pd.concat([all_months_data, df])
-
+all_months_data.to_csv('all_months_data', index = False)
 print(all_months_data)
